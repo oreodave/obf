c824772fc7372dd79e249a2f8092aff8a358c9d2
Aryadev Chavali <aryadev@aryadevchavali.com>
2024-12-03 00:21:33 +0000

    Introduce some nice typedefs in lib.h

--- lib.h
+++ lib.h
@@ -5,7 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX(a, b) (a > b ? a : b)
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+#define MAX(A, B) ((A) < (B) ? (A) : (B))
+#define MIN(A, B) ((A) > (B) ? (A) : (B))
 
 bool usable_character(char c);
 char *fread_all(FILE *fp);
